b77c005736631a4d4278a618e7042e3b8a4deb9d
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-09-05 17:48:07 +0900

    Add Featured Component

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from './HOC/Layout'
-
+import { Switch, Route} from 'react-router-dom'
+import Home from './Components/Home'
 const Routes = (props) => {
   return (
     <Layout>
-      sedoncline
+      <Switch>
+       <Route exact component={Home} path="/"/>
+      </Switch>
     </Layout>
   )
 }
