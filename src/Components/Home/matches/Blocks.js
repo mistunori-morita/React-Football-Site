882bbc4dae755dffdf146d487a0519eda760015a
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-09-07 19:53:12 +0900

    Matched Component comp

--- src/Components/Home/matches/Blocks.js
+++ src/Components/Home/matches/Blocks.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react'
 import { firebaseMatches } from '../../../firebase'
-import { firebaseLooper, reverseArray } from "../../../Ui/misc";
+import { firebaseLooper, reverseArray } from "../../../Ui/misc"
+import MatchesBlock from '../../../Ui/matches_block'
+import Slide from "react-reveal/Slide"
+
 
 
 export default class Blocks extends Component {
-
-
   state = {
     matches: []
   }
 
-
   componentDidMount = () => {
     firebaseMatches.limitToLast(6).once('value')
     .then((snapshot)=> {
-      const m = firebaseLooper(snapshot);
+      const matches = firebaseLooper(snapshot);
 
-      this.setState({ matches: reverseArray(m) });
+      this.setState({ matches: reverseArray(matches) });
     })
   }
 
-  showMatches = () => (
-    <div>
-      match
-    </div>
+  showMatches = (matches) => (
+   matches ?
+      matches.map((index)=>(
+        <Slide bottom key={index}>
+          <div className="item">
+            <div className="wrapper">
+              <MatchesBlock match={index}/>
+            </div>
+          </div>
+        </Slide>
+      ))
+    : null
   )
 
 
